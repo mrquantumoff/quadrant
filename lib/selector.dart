0a5ed33ec7ba35ee11c8b6f0d05fa182c62d3532
Demir Yerli <mrquantumoff@protonmail.com>
2023-04-09 00:33:16 +0300

    get rid of useless imports

--- lib/selector.dart
+++ lib/selector.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:mcmodpackmanager_reborn/backend.dart';
 import 'package:mcmodpackmanager_reborn/modpack_creator/modpack_creator.dart';
 
