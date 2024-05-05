c396bb27e340a44284e61abe0e31dc540997a110
Demir Yerli <mrquantumoff@protonmail.com>
2024-05-05 15:30:25 +0300

    v24.5.3

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,7 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
   flutter_platform_alert
   flutter_secure_storage_windows
-  protocol_handler
+  protocol_handler_windows
   screen_retriever
   url_launcher_windows
   window_manager
