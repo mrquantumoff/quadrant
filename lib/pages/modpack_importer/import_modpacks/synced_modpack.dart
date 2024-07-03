88e0c310db48952f050446b2d7026c338696bc33
Demir Yerli <mrquantumoff@protonmail.com>
2024-07-03 22:26:52 +0300

    add the option to view the owners

--- lib/pages/modpack_importer/import_modpacks/synced_modpack.dart
+++ lib/pages/modpack_importer/import_modpacks/synced_modpack.dart
@@ -59,130 +59,220 @@ class _SyncedModpackState extends State<SyncedModpack> {
       }
     }
 
-    return SizedBox(
-      width: 640,
-      height: 192,
-      child: Card(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
+    return Card(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 24),
+        child: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.only(left: 12, top: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.name,
-                    style: const TextStyle(fontSize: 36),
-                  ),
-                  Text(
-                    "${widget.modLoader} ${widget.mcVersion}",
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.cloudSyncDate(date),
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!
-                        .localSyncDate(localSyncedLocalDate),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.only(right: 12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    FilledButton.icon(
-                      onPressed: () async {
-                        List mods = json.decode(widget.mods);
-                        debugPrint(mods.toString());
-                        Map modConfig = {
-                          "name": widget.name,
-                          "version": widget.mcVersion,
-                          "mods": mods,
-                          "modLoader": widget.modLoader
-                        };
-                        if (widget.getMods != null) {
-                          await widget.getMods!(json.encode(modConfig),
-                              switchTabs: true);
-                        }
-                      },
-                      icon: const Icon(Icons.download),
-                      label: Text(AppLocalizations.of(context)!.download),
-                      style: FilledButton.styleFrom(
-                        minimumSize: const Size(360, 48),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 12, top: 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.name,
+                        style: const TextStyle(fontSize: 36),
                       ),
+                      Text(
+                        "${widget.modLoader} ${widget.mcVersion}",
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        AppLocalizations.of(context)!.cloudSyncDate(date),
+                      ),
+                      Text(
+                        AppLocalizations.of(context)!
+                            .localSyncDate(localSyncedLocalDate),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        FilledButton.icon(
+                          onPressed: () async {
+                            List mods = json.decode(widget.mods);
+                            debugPrint(mods.toString());
+                            Map modConfig = {
+                              "name": widget.name,
+                              "version": widget.mcVersion,
+                              "mods": mods,
+                              "modLoader": widget.modLoader
+                            };
+                            if (widget.getMods != null) {
+                              await widget.getMods!(json.encode(modConfig),
+                                  switchTabs: true);
+                            }
+                          },
+                          icon: const Icon(Icons.download),
+                          label: Text(AppLocalizations.of(context)!.download),
+                          style: FilledButton.styleFrom(
+                            minimumSize: const Size(360, 48),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        FilledButton.icon(
+                          onPressed: () async {
+                            Map modConfig = {
+                              "name": widget.name,
+                              "version": widget.mcVersion,
+                              "mods": json.decode(widget.mods),
+                              "modLoader": widget.modLoader
+                            };
+                            String content = json.encode(modConfig);
+                            await shareModpack(context, content);
+                          },
+                          label: Text(AppLocalizations.of(context)!.share),
+                          icon: const Icon(Icons.share),
+                          style: FilledButton.styleFrom(
+                            backgroundColor: Colors.blueAccent,
+                            foregroundColor: Colors.white,
+                            minimumSize: const Size(360, 48),
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    FilledButton.icon(
-                      onPressed: () async {
-                        http.Response res = await http.delete(
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                top: 16,
+              ),
+              child: ExpansionTile(
+                maintainState: true,
+                expandedAlignment: Alignment.centerLeft,
+                title: Text(
+                  AppLocalizations.of(context)!.details,
+                  style: const TextStyle(fontSize: 24),
+                ),
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 32),
+                    child: FutureBuilder(
+                      future: () async {
+                        http.Response res = await http.get(
                           Uri.parse(
-                              "https://api.mrquantumoff.dev/api/v2/delete/quadrant_sync"),
+                              "https://api.mrquantumoff.dev/api/v3/quadrant/sync/get?show_owners=true&modpack_id=${widget.modpackId}"),
                           headers: {
                             "User-Agent": await generateUserAgent(),
                             "Authorization": "Bearer ${widget.token}"
                           },
-                          body: json.encode(
-                            {
-                              "modpack_id": widget.modpackId,
-                              "name": widget.name,
-                            },
-                          ),
                         );
-                        if (res.statusCode != 200) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(res.body),
+                        return res;
+                      }(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text(
+                              snapshot.error.toString(),
                             ),
                           );
                         }
-                        File modpackSyncFile = File(
-                            "${getMinecraftFolder().path}/modpacks/${widget.name}/quadrantSync.json");
-                        if (modpackSyncFile.existsSync()) {
-                          await modpackSyncFile.delete();
+                        List<dynamic> modpack_raw =
+                            json.decode(snapshot.data!.body);
+                        Map modpack = modpack_raw[0];
+                        List<dynamic> owners = modpack["owners"];
+                        List<Widget> ownersWidgets = [];
+                        for (var owner in owners) {
+                          ownersWidgets.add(
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8),
+                              child: Text(
+                                owner["admin"]
+                                    ? AppLocalizations.of(context)!
+                                        .owner(owner["username"])
+                                    : owner["username"],
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          );
                         }
-                        widget.reload();
+
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                                  Text(
+                                    "${AppLocalizations.of(context)!.owners}:",
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                ] +
+                                ownersWidgets +
+                                [
+                                  FilledButton.icon(
+                                    onPressed: () async {
+                                      http.Response res = await http.delete(
+                                        Uri.parse(
+                                            "https://api.mrquantumoff.dev/api/v2/delete/quadrant_sync"),
+                                        headers: {
+                                          "User-Agent":
+                                              await generateUserAgent(),
+                                          "Authorization":
+                                              "Bearer ${widget.token}"
+                                        },
+                                        body: json.encode(
+                                          {
+                                            "modpack_id": widget.modpackId,
+                                            "name": widget.name,
+                                          },
+                                        ),
+                                      );
+                                      if (res.statusCode != 200) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          SnackBar(
+                                            content: Text(res.body),
+                                          ),
+                                        );
+                                      }
+                                      File modpackSyncFile = File(
+                                          "${getMinecraftFolder().path}/modpacks/${widget.name}/quadrantSync.json");
+                                      if (modpackSyncFile.existsSync()) {
+                                        await modpackSyncFile.delete();
+                                      }
+                                      widget.reload();
+                                    },
+                                    label: Text(AppLocalizations.of(context)!
+                                        .leaveOrDelete),
+                                    icon: const Icon(Icons.delete),
+                                    style: FilledButton.styleFrom(
+                                      backgroundColor: Colors.redAccent,
+                                      foregroundColor: Colors.white,
+                                      minimumSize: const Size(360, 48),
+                                    ),
+                                  ),
+                                ],
+                          ),
+                        );
                       },
-                      label: Text(AppLocalizations.of(context)!.delete),
-                      icon: const Icon(Icons.delete),
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.redAccent,
-                        foregroundColor: Colors.white,
-                        minimumSize: const Size(360, 48),
-                      ),
                     ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    FilledButton.icon(
-                      onPressed: () async {
-                        Map modConfig = {
-                          "name": widget.name,
-                          "version": widget.mcVersion,
-                          "mods": json.decode(widget.mods),
-                          "modLoader": widget.modLoader
-                        };
-                        String content = json.encode(modConfig);
-                        await shareModpack(context, content);
-                      },
-                      label: Text(AppLocalizations.of(context)!.share),
-                      icon: const Icon(Icons.share),
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.blueAccent,
-                        foregroundColor: Colors.white,
-                        minimumSize: const Size(360, 48),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
