e37aa9883a64c7be0e2c14ef057241957df73cbc
Demir Yerli <mrquantumoff@protonmail.com>
2024-07-07 16:23:59 +0300

    v24.7.2

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -29,7 +29,7 @@ static void my_application_activate(GApplication *application)
   // in case the window manager does more exotic layout, e.g. tiling.
   // If running on Wayland assume the header bar will work (may need changing
   // if future cases occur).
-  gboolean use_header_bar = FALSE;
+  gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
   GdkScreen *screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen))
@@ -46,7 +46,7 @@ static void my_application_activate(GApplication *application)
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
     gtk_header_bar_set_title(header_bar, "Quadrant");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
+    gtk_header_bar_set_show_close_button(header_bar, FALSE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else
