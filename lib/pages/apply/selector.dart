42d2655d1e3111fcda950cd1dd6c7073a472692c
Demir Yerli <mrquantumoff@protonmail.com>
2024-07-11 20:31:30 +0300

    make quadrant share the 3rd button in the selector

--- lib/pages/apply/selector.dart
+++ lib/pages/apply/selector.dart
@@ -50,9 +50,9 @@ class _SelectorState extends State<Selector> {
         updateOptions();
       },
     );
-    // updateOptionsTimer = Timer.periodic(const Duration(seconds: 5), (timer) {
-    //   updateOptions();
-    // });
+    updateOptionsTimer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      updateOptions();
+    });
   }
 
   @override
@@ -87,152 +87,154 @@ class _SelectorState extends State<Selector> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: FilledButton(
-                  onPressed: areButtonsActive
-                      ? () {
-                          setState(
-                            () {
-                              areButtonsActive = false;
-                            },
-                          );
-                          bool res = applyModpack(selectedModpack);
-                          if (res) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: const Duration(seconds: 1),
-                                content: Text(AppLocalizations.of(context)!
-                                    .setModpackSuccess),
-                              ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: const Duration(seconds: 1),
-                                content: Text(AppLocalizations.of(context)!
-                                    .setModpackFailed),
-                              ),
-                            );
-                          }
-                          setState(
-                            () {
-                              areButtonsActive = true;
-                            },
-                          );
-                        }
-                      : () {
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: FilledButton(
+                onPressed: areButtonsActive
+                    ? () {
+                        setState(
+                          () {
+                            areButtonsActive = false;
+                          },
+                        );
+                        bool res = applyModpack(selectedModpack);
+                        if (res) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               duration: const Duration(seconds: 1),
-                              content: Text(
-                                AppLocalizations.of(context)!
-                                    .buttonsAreDisabled,
-                              ),
+                              content: Text(AppLocalizations.of(context)!
+                                  .setModpackSuccess),
                             ),
                           );
-                        },
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Row(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(right: 5, left: 0),
-                          child: Icon(Icons.check),
-                        ),
-                        Text(AppLocalizations.of(context)!.apply)
-                      ],
-                    ),
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              duration: const Duration(seconds: 1),
+                              content: Text(AppLocalizations.of(context)!
+                                  .setModpackFailed),
+                            ),
+                          );
+                        }
+                        setState(
+                          () {
+                            areButtonsActive = true;
+                          },
+                        );
+                      }
+                    : () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            duration: const Duration(seconds: 1),
+                            content: Text(
+                              AppLocalizations.of(context)!.buttonsAreDisabled,
+                            ),
+                          ),
+                        );
+                      },
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 5, left: 0),
+                        child: Icon(Icons.check),
+                      ),
+                      Text(AppLocalizations.of(context)!.apply)
+                    ],
                   ),
                 ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: FilledButton.tonal(
-                  onPressed: areButtonsActive
-                      ? () {
-                          setState(
-                            () {
-                              areButtonsActive = false;
-                            },
-                          );
-                          bool res = clearModpack();
-                          if (res) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: const Duration(seconds: 1),
-                                content: Text(AppLocalizations.of(context)!
-                                    .setModpackSuccess),
-                              ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: const Duration(seconds: 1),
-                                content: Text(AppLocalizations.of(context)!
-                                    .setModpackFailed),
-                              ),
-                            );
-                          }
-                          setState(
-                            () {
-                              areButtonsActive = true;
-                            },
-                          );
-                          updateOptions();
-                        }
-                      : () {
+            const SizedBox(
+              width: 12,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: FilledButton.tonal(
+                onPressed: areButtonsActive
+                    ? () {
+                        setState(
+                          () {
+                            areButtonsActive = false;
+                          },
+                        );
+                        bool res = clearModpack();
+                        if (res) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               duration: const Duration(seconds: 1),
-                              content: Text(
-                                AppLocalizations.of(context)!
-                                    .buttonsAreDisabled,
-                              ),
+                              content: Text(AppLocalizations.of(context)!
+                                  .setModpackSuccess),
                             ),
                           );
-                        },
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Row(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(right: 5, left: 0),
-                          child: Icon(Icons.delete),
-                        ),
-                        Text(AppLocalizations.of(context)!.clear)
-                      ],
-                    ),
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              duration: const Duration(seconds: 1),
+                              content: Text(AppLocalizations.of(context)!
+                                  .setModpackFailed),
+                            ),
+                          );
+                        }
+                        setState(
+                          () {
+                            areButtonsActive = true;
+                          },
+                        );
+                        updateOptions();
+                      }
+                    : () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            duration: const Duration(seconds: 1),
+                            content: Text(
+                              AppLocalizations.of(context)!.buttonsAreDisabled,
+                            ),
+                          ),
+                        );
+                      },
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 5, left: 0),
+                        child: Icon(Icons.delete),
+                      ),
+                      Text(AppLocalizations.of(context)!.clear)
+                    ],
                   ),
                 ),
               ),
             ),
-            // Container(
-            //   margin: const EdgeInsets.symmetric(horizontal: 5),
-            //   child: Padding(
-            //     padding: const EdgeInsets.symmetric(vertical: 15),
-            //     child: ElevatedButton(
-            //       onPressed: updateOptions,
-            //       child: Padding(
-            //         padding: const EdgeInsets.all(15),
-            //         child: Row(
-            //           children: [
-            //             const Padding(
-            //               padding: EdgeInsets.only(right: 5, left: 0),
-            //               child: Icon(Icons.refresh),
-            //             ),
-            //             Text(AppLocalizations.of(context)!.reload)
-            //           ],
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
+            const SizedBox(
+              width: 12,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (selectedModpack == null) return;
+                  File modpackConfig = File(
+                      "${getMinecraftFolder().path}/modpacks/$selectedModpack/modConfig.json");
+                  if (!modpackConfig.existsSync()) return;
+                  String content = await modpackConfig.readAsString();
+                  shareModpack(context, content);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 5, left: 0),
+                        child: Icon(Icons.share),
+                      ),
+                      Text(AppLocalizations.of(context)!.share)
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
         // Open modpacks folder / Export selected modpack buttons
@@ -262,42 +264,16 @@ class _SelectorState extends State<Selector> {
                   if (!modpackConfig.existsSync()) return;
                   String content = await modpackConfig.readAsString();
 
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title:
-                            Text(AppLocalizations.of(context)!.exportOptions),
-                        actions: [
-                          TextButton(
-                            onPressed: () async {
-                              var filePickerResult = await FilePicker.platform
-                                  .saveFile(fileName: "$selectedModpack.json");
-                              if (filePickerResult == null) return;
-                              File selectedFile = File(filePickerResult);
-                              if (await selectedFile.exists()) {
-                                await selectedFile.delete(recursive: true);
-                              }
-                              await selectedFile.create(recursive: true);
+                  var filePickerResult = await FilePicker.platform
+                      .saveFile(fileName: "$selectedModpack.json");
+                  if (filePickerResult == null) return;
+                  File selectedFile = File(filePickerResult);
+                  if (await selectedFile.exists()) {
+                    await selectedFile.delete(recursive: true);
+                  }
+                  await selectedFile.create(recursive: true);
 
-                              await selectedFile.writeAsString(content);
-                            },
-                            child: Text(
-                              AppLocalizations.of(context)!.referenceFile,
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              shareModpack(context, content);
-                            },
-                            child: Text(
-                              AppLocalizations.of(context)!.manualInput,
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  );
+                  await selectedFile.writeAsString(content);
                 },
                 label: Text(AppLocalizations.of(context)!.exportMods),
                 avatar: const Icon(Icons.upload_file_outlined),
